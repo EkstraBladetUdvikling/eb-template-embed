ee43ce1a538cf8a1552ba62c20bc46a534541a40
Mikkel B. Jensen <mikkel.jensen@eb.dk>
2016-12-22 15:49:23 +0100

    Added GA

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,6 +3,20 @@
 /* ES6 import */
 import testFunc from './_importme'
 
+/*
+let initialInteraction = true
+
+if (typeof ga !== 'undefined' && initialInteraction) {
+  ga('send', { // eslint-disable-line no-undef
+    hitType: 'event',
+    eventCategory: 'Unnamed embed',
+    eventAction: 'initial Interaction',
+    eventLabel: 'embed'
+  })
+  initialInteraction = false
+}
+*/
+
 (function () {
   let ecmascriptest = 'world'
   const header = document.getElementsByClassName('article-widget-header--text')[0]
