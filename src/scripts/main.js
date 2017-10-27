2407a3108d20c9ec58f4fae6f7da18fbdab68deb
mbjedk <anonymtbrev@gmail.com>
2017-10-27 11:24:18 +0200

    Small changes in main

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,21 +1,5 @@
-'use strict'
-
 /* ES6 import */
 import testFunc from './_importme'
-
-/*
-let initialInteraction = true
-
-if (typeof ga !== 'undefined' && initialInteraction) {
-  ga('send', { // eslint-disable-line no-undef
-    hitType: 'event',
-    eventCategory: 'Unnamed embed',
-    eventAction: 'initial Interaction',
-    eventLabel: 'embed'
-  })
-  initialInteraction = false
-}
-*/
 
 window.ebmodule = (function () {
   let privateProperty = 'foo'
