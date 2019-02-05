fe4dc21298c79d90b439d1a7d33883794f057746
mbjedk <anonymtbrev@gmail.com>
2019-02-05 10:28:18 +0100

    Added tracking, parent height setting, removed font-awesome, removed minify-svg in HTML

--- src/scripts/main.ts
+++ src/scripts/main.ts
@@ -1,9 +1,51 @@
 class EbFeatureClass {
+  private productionName = 'eb-unnamed'; // Set DIV id to productionname to enable height
+  private trackingData = {
+    eventAction: '2019-' + this.productionName,
+    eventCategory: 'EB interaktiv',
+    eventLabel: 'First interaction',
+    eventNint: 0,
+    eventValue: 0
+  };
+  private firstInteraction: boolean = false;
+  private completeInteraction: boolean = false;
+
+  /**
+   * Init production
+   */
   constructor() {
-    const titleTextElem: HTMLElement = document.querySelector(
-      '.article-widget-header--text'
+    this.updateHeight();
+  }
+
+  /**
+   * Tracks the first interaction once
+   */
+  private trackFirstInteraction(): void {
+    if (!this.firstInteraction) {
+      parent.postMessage(this.trackingData, '*');
+      this.firstInteraction = true;
+    }
+  }
+
+  /**
+   * Tracks the final / complete action once
+   */
+  private trackCompleteInteraction(): void {
+    if (!this.completeInteraction) {
+      this.trackingData.eventLabel = 'Completed interaction';
+      parent.postMessage(this.trackingData, '*');
+      this.completeInteraction = true;
+    }
+  }
+
+  /**
+   * Update height on parent
+   */
+  private updateHeight(): void {
+    parent.postMessage(
+      'token:#' + this.productionName + ',height:' + String(document.documentElement.offsetHeight),
+      '*'
     );
-    titleTextElem.innerText = 'Test Title';
   }
 }
 
